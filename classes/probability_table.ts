5354779a331a6dc6eeb034f0744f047896baf708
Nuray <nuraynurzhaskyzy@gmail.com>
2025-05-27 17:17:37 +0500

    Add table

--- classes/probability_table.ts
+++ classes/probability_table.ts
@@ -1,25 +1,31 @@
 import { Dice } from "./dice";
 import { ProbabilityCalculator } from "./probability_calculator";
+import Table from "cli-table3";
 
 export class ProbabilityTable {
   static buildTable(dice: Dice[]) {
     const size = dice.length;
-    const header = ["     ", ...dice.map((die, i) => `D${i + 1}`)].join(" | ");
-    const separator = "-".repeat(header.length + size * 3);
-    console.log(header);
-    console.log(separator);
+
+    const table = new Table({
+      head: [""].concat(dice.map((_, i) => `D${i + 1}`)),
+      colWidths: new Array(size + 1).fill(12),
+      wordWrap: true,
+    });
 
     for (let i = 0; i < size; i++) {
       const row = [`D${i + 1}`];
       for (let j = 0; j < size; j++) {
         if (i === j) {
-          row.push("   - ");
+          row.push("-");
         } else {
           const { a } = ProbabilityCalculator.calculate(dice[i], dice[j]);
           row.push(`${a}%`);
         }
       }
-      console.log(row.join(" | "));
+      table.push(row);
     }
+
+    console.log("Probability of the win for the user:");
+    console.log(table.toString());
   }
 }
